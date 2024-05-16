ebadd1cefe408fbd28467e0a8e481a56eef9a7f1
Gladis Wilma Stockhausenn <gladiswilmaStock@gmail.com>
2024-05-16 17:02:18 -0300

    Pipes criadas

--- controle-receitas-despesas/src/app/paginas/extrato/extrato.component.ts
+++ controle-receitas-despesas/src/app/paginas/extrato/extrato.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { DataPipe } from '../../data.pipe';
 
 @Component({
   selector: 'app-extrato',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, DataPipe],
   templateUrl: './extrato.component.html',
   styleUrl: './extrato.component.scss'
 })
 export class ExtratoComponent {
+  
+  public date = new Date();
+  public salario = 12000;
 
 }
