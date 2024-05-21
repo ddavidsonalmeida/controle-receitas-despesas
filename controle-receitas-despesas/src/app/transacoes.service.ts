a69bea57b8fc4dfee80315525f64d288486f4783
Davidson <davidson.android10@gmail.com>
2024-05-21 13:10:31 -0300

    service configurada

--- controle-receitas-despesas/src/app/transacoes.service.ts
+++ controle-receitas-despesas/src/app/transacoes.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Transacao {
+  id: number;
+  descricao: string;
+  tipo: string;
+  valor: number;
+  data: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransacoesService {
 
-  constructor() { }
+  private url = 'http://localhost:3000/transacoes';
+
+  constructor(private http: HttpClient) { }
+
+  getTransacoes(): Observable<Transacao[]> {
+    return this.http.get<Transacao[]>(this.url);
+  }
 }
