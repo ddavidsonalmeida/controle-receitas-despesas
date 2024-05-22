7a0a0c88cf863af761b2094a44ecc0682d37d8b0
Fabio Diniz <difreitas.arq.urb@gmail.com>
2024-05-22 16:28:22 -0300

    Ajuste dos cards

--- controle-receitas-despesas/src/app/paginas/dicas-investimentos/dicas-investimentos.component.ts
+++ controle-receitas-despesas/src/app/paginas/dicas-investimentos/dicas-investimentos.component.ts
@@ -10,19 +10,19 @@ import { Component } from '@angular/core';
 export class DicasInvestimentosComponent {
 listaDeCard = [
   {
-    titulo: 'Glossário definitivo do mercado financeiro: entenda os termos.', imagem: './../../../assets/imagens/touro-merc-financeiro.png', texto:'kjsdhfdiugchairufhbiouqwhefiohciweo'
+    titulo: 'Glossário definitivo do mercado financeiro: entenda os termos.', imagem: './../../../assets/imagens/touro-merc-financeiro.png', texto:'aqui entra o texto'
   },
   {
-    titulo: 'Como trabalhar na bolsa de valores? Quanto ganha?', imagem: './../../../assets/imagens/como-trabalhar-na-bolsa.png', texto:'kjjhgfugawrhiofhjpoweforuthrtrhtrfhbiouqwhefiohciweo'
+    titulo: 'Como trabalhar na bolsa de valores? Quanto ganha?', imagem: './../../../assets/imagens/como-trabalhar-na-bolsa.png', texto:'aqui entra o texto'
   },
   {
-    titulo: 'Aprenda a diferença entre assessor e consultor de inves...', imagem: './../../../assets/imagens/aprenda-a-diferenca.png', texto:'kjjhgfugawrhiofhjsthsthpoweforufhbiouqwhefiohciweo'
+    titulo: 'Aprenda a diferença entre assessor e consultor de inves...', imagem: './../../../assets/imagens/aprenda-a-diferenca.png', texto:'aqui entra o texto'
   },
   {
-    titulo: 'O que é CPA 10? Tudo sobre essa certificação', imagem: './../../../assets/imagens/o-que-e-cpa-10.png', texto:'kjjhgfugawrhiofhjpowefogfhdtfhrufhbiouqwhefiohciweo'
+    titulo: 'O que é CPA 10? Tudo sobre essa certificação', imagem: './../../../assets/imagens/o-que-e-cpa-10.png', texto:'aqui entra o texto'
   },
   {
-    titulo: 'Melhores formas de investir o seu 13º salário.', imagem: './../../../assets/imagens/melhor-forma-de-investir.png', texto:'kjjhgfugawrhiofhjpoweforufhbiouqwhefiohciweo'
+    titulo: 'Melhores formas de investir o seu 13º salário.', imagem: './../../../assets/imagens/melhor-forma-de-investir.png', texto:'aqui entra o texto'
   },
   {
     titulo: 'Como investir em CDB? Tudo o que você precisa saber sobre...', imagem: './../../../assets/imagens/como-investir-em-cdb.png', texto:'kjjhgfugawrhiofhjpowesththforufhbiouqwhefiohciweo'
