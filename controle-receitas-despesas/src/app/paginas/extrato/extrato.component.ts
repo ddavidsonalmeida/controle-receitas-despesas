a0c294a19e8ba5c3ac8882749414aec9ae154aea
Davidson <davidson.android10@gmail.com>
2024-05-26 12:06:42 -0300

    Lista transacoes-funcionando

--- controle-receitas-despesas/src/app/paginas/extrato/extrato.component.ts
+++ controle-receitas-despesas/src/app/paginas/extrato/extrato.component.ts
@@ -4,79 +4,61 @@ import { Component, OnInit } from '@angular/core';
 import { NgStyle, NgIf, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-
 import { TransacoesService } from '../../transacoes.service';
-import { Transacao } from './../../transacoes';
+import { Transacao } from '../../model/transacoes';
 
 import { CurrencyPipe } from '@angular/common';
 import { DataPipe } from '../../data.pipe';
 import { DicasInvestimentosComponent } from '../dicas-investimentos/dicas-investimentos.component';
 import { DicasRegularizacaoComponent } from '../dicas-regularizacao/dicas-regularizacao.component';
 
-
-
 @Component({
   selector: 'app-extrato',
   standalone: true,
-  imports: [CurrencyPipe, DataPipe, NgStyle, NgIf, NgFor, DicasInvestimentosComponent, DicasRegularizacaoComponent, RouterLink],
+  imports: [
+    CurrencyPipe,
+    DataPipe,
+    NgStyle,
+    NgIf,
+    NgFor,
+    RouterLink,
+    DicasInvestimentosComponent,
+    DicasRegularizacaoComponent
+  ],
   templateUrl: './extrato.component.html',
-  styleUrl: './extrato.component.scss'
+  styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
   public date = new Date();
   salario: number = 12000;
   receitas: number = 6000;
   despesas: number = 3000;
-  transacoes: any[] = [];
-  transacoesFiltradas: any[] = [];
+  transacoes: Transacao[] = [];
+  transacoesFiltradas: Transacao[] = [];
   tipoSelecionado: string = '';
 
-  // transacoes: Transacao[] = [];
-
-  constructor(private TransacoesService: TransacoesService) { }
+  constructor(private transacoesService: TransacoesService) {}
 
   ngOnInit(): void {
     this.getTransacoes();
-    
-
   }
 
   getTransacoes(): void {
-    this.TransacoesService.getTransacoes()
-      .subscribe(transacoes => this.transacoes = transacoes);
-
-
+    this.transacoesService.getTransacoes()
+      .subscribe(transacoes => {
+        this.transacoes = transacoes;
+        this.transacoesFiltradas = transacoes;
+      });
   }
 
-
-  // constructor(){}
-
-  saldo() {
+  saldo(): number {
     return this.receitas - this.despesas;
   }
 
-  treceitas: number = 6000;
-
-  tdespesas: number = 3500;
-
-//Esta funcão deveria calcular o valor e devidir se levara para a tela de investimento ou para a tela de regularização, mas esta com erro que não identifiquei
-
-  // ObterRotaComBaseNoSaldo(): { rota: string, texto: string } {
-  //   if (this.saldo < this.salario * 0.5) {
-  //     return { rota: '/dicas-regularizacao', texto: 'Regularização' };
-  //   } else if (this.saldo >= this.salario * 3) {
-  //     return { rota: '/dicas-investimentos', texto: 'Investimentos' };
-  //   } else {
-  //     return { rota: '', texto: 'Botão Desabilitado' };
-  //   }
-  // }
-
-
   carregarTransacoes(): void {
-
-    this.TransacoesService.getTransacoes().subscribe((data: any[]) => {
+    this.transacoesService.getTransacoes().subscribe((data: Transacao[]) => {
       this.transacoes = data;
-      this.transacoesFiltradas = this.transacoes;
+      this.transacoesFiltradas = data;
     });
   }
 
@@ -90,5 +72,3 @@ export class ExtratoComponent implements OnInit {
     }
   }
 }
-  
-
